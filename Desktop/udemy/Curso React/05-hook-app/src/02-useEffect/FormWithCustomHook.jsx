0f22e768f326c898f842fe118d13f8b71f7af9d5
sonifeg <l.d.i-sfg@hotmail.com>
2023-06-08 12:04:32 -0600

    forms and custom hook of forms

---
+++ Desktop/udemy/Curso React/05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,42 @@
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHook = () => {
+  const { formState, onInputChange, username, email, password } = useForm({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  //const { username, email, password } = formState;
+
+  return (
+    <>
+      <h1>Custom Hook Form</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="Username@gmail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+      <input
+        type="password"
+        className="form-control mt-2"
+        placeholder="password"
+        name="password"
+        value={password}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
---
+++ Desktop/udemy/Curso React/05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Message } from "./Message";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "Sonia",
+    email: "sonia@gmail.com",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    //console.log("useEffect called");
+  }, []);
+
+  useEffect(() => {
+    //console.log("formState changed");
+  }, [formState]);
+
+  useEffect(() => {
+    //console.log("email changed");
+  }, [email]);
+
+  return (
+    <>
+      <h1>SimpleForm</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="Username@gmail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+      {username === "Sonia" && <Message />}
+    </>
+  );
+};
