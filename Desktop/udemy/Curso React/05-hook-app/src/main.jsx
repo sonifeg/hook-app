18be56cadf39d8e4ddc3ee184b58f23f4194ed54
sonifeg <l.d.i-sfg@hotmail.com>
2023-06-08 09:55:38 -0600

    useState hoook and custome useCounter

--- Desktop/udemy/Curso React/05-hook-app/src/main.jsx
+++ Desktop/udemy/Curso React/05-hook-app/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import { CounterApp } from "./01-useState/CounterApp";
+// import { HooksApp } from "./HooksApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CounterWithCustomHook />
+  </React.StrictMode>
+);
